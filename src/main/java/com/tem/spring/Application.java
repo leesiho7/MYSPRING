675548e0aabf995918afbeb75e37453c8981f18b
leesiho7 <leesiho58@gmail.com>
2026-08-24 01:22:50 +0900

    config: switch back to MySQL datasource with verified connection on port 3306

--- src/main/java/com/tem/spring/Application.java
+++ src/main/java/com/tem/spring/Application.java
@@ -2,14 +2,10 @@ package com.tem.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class,
 		org.springframework.ai.autoconfigure.vectorstore.chroma.ChromaVectorStoreAutoConfiguration.class
 })
 public class Application {
